61a5cf82ee5716c07f92ad605d3c09d6719b95a1
bozeeetopic <btopic00@fesb.hr>
2021-12-18 19:47:23 +0100

    Changed few column names, did a lot of inserts

--- DatabaseQuery.sql
+++ DatabaseQuery.sql
@@ -5,8 +5,8 @@ USE LanguageSchoolDatabase
 
 CREATE TABLE Teachers(
 	Id int IDENTITY(1,1) PRIMARY KEY,
-	[Name] nvarchar(30) NOT NULL,
-	Surname nvarchar(30) NOT NULL
+	TeacherName nvarchar(30) NOT NULL,
+	TeacherSurname nvarchar(30) NOT NULL
 )
 CREATE TABLE Classrooms(
 	Id int IDENTITY(1,1) PRIMARY KEY,
@@ -14,9 +14,11 @@ CREATE TABLE Classrooms(
 )
 CREATE TABLE Courses (
 	Id int IDENTITY(1,1) PRIMARY KEY,
-	[Language] nvarchar(30) NOT NULL UNIQUE,
+	CourseLanguage nvarchar(30) NOT NULL,
+	DifficultyLevel int NOT NULL,
 	TotalPrice bigint NOT NULL,
-	ClassroomId int FOREIGN KEY REFERENCES Classrooms(Id) NOT NULL
+	ClassroomId int FOREIGN KEY REFERENCES Classrooms(Id) NOT NULL,
+	CONSTRAINT Uniqueness UNIQUE (DifficultyLevel,CourseLanguage)
 )
 CREATE TABLE TeacherCourse (
 	Id int IDENTITY(1,1) PRIMARY KEY,
@@ -25,14 +27,14 @@ CREATE TABLE TeacherCourse (
 )
 CREATE TABLE Lectures (
 	Id int IDENTITY(1,1) PRIMARY KEY,
-	[Date] datetime2 NOT NULL,
+	DateOfLecture datetime2 NOT NULL,
 	DurationMinutes int NOT NULL,
 	TeacherCourseId int FOREIGN KEY REFERENCES TeacherCourse(Id) NOT NULL
 )
 CREATE TABLE Atendees (
 	Id int IDENTITY(1,1) PRIMARY KEY,
-	[Name] nvarchar(30) NOT NULL,
-	Surname nvarchar(30) NOT NULL,
+	AtendeeName nvarchar(30) NOT NULL,
+	AtendeeSurname nvarchar(30) NOT NULL,
 	Age int CHECK (Age > 0 AND Age < 100) NOT NULL
 )
 CREATE TABLE LectureAtendee (
@@ -42,7 +44,7 @@ CREATE TABLE LectureAtendee (
 	LectureId int FOREIGN KEY REFERENCES Lectures(Id) NOT NULL
 )
 
-INSERT INTO Teachers([Name],Surname) VALUES
+INSERT INTO Teachers(TeacherName,TeacherSurname) VALUES
 ('Emmery','Crosse'),
 ('Dionisio','Reedman'),
 ('Skylar','Henningham'),
@@ -66,15 +68,31 @@ INSERT INTO Classrooms(RoomNumber) VALUES
 ('898'),
 ('456'),
 ('112')
-INSERT INTO Courses([Language],TotalPrice,ClassroomId) VALUES
-('Bulgarian',3000,1),
-('Chinese',9000,1),
-('Indonesian',12000,2),
-('Russian',120,3),
-('Uzbekistanian',130,4),
-('Polish',5,5),
-('Colombian',8000,6),
-('Korean',56000,7)
+INSERT INTO Courses(CourseLanguage,DifficultyLevel,TotalPrice,ClassroomId) VALUES
+('Bulgarian',1,3000,1),
+('Chinese',1,9000,1),
+('Indonesian',1,12000,2),
+('Russian',1,120,3),
+('Uzbekistanian',1,130,4),
+('Polish',1,5,5),
+('Colombian',1,8000,6),
+('Korean',1,56000,7),
+('Bulgarian',2,3000,1),
+('Chinese',2,9000,1),
+('Indonesian',2,12000,2),
+('Russian',2,120,3),
+('Uzbekistanian',2,130,4),
+('Polish',2,5,5),
+('Colombian',2,8000,6),
+('Korean',2,56000,7),
+('Chinese',3,9000,1),
+('Indonesian',3,12000,2),
+('Russian',3,120,3),
+('Uzbekistanian',3,130,4),
+('Colombian',3,8000,6),
+('Korean',3,56000,7),
+('Chinese',4,9000,1),
+('Korean',4,9000,1)
 INSERT INTO TeacherCourse(TeacherId,CourseId) VALUES
 (1,1),
 (2,1),
@@ -91,8 +109,30 @@ INSERT INTO TeacherCourse(TeacherId,CourseId) VALUES
 (8,7),
 (8,8),
 (9,8),
-(10,8)
-INSERT INTO Lectures([Date],DurationMinutes,TeacherCourseId) VALUES
+(10,8),
+(1,9),
+(2,10),
+(3,10),
+(2,11),
+(3,12),
+(2,13),
+(3,14),
+(3,15),
+(4,15),
+(5,15),
+(6,16),
+(7,16),
+(8,17),
+(8,18),
+(9,18),
+(10,18),
+(1,19),
+(2,19),
+(3,20),
+(2,21),
+(3,22),
+(2,23)
+INSERT INTO Lectures(DateOfLecture,DurationMinutes,TeacherCourseId) VALUES
 ('2021-06-19',86,8),
 ('2021-10-27',108,2),
 ('2021-09-14',94,3),
@@ -122,8 +162,38 @@ INSERT INTO Lectures([Date],DurationMinutes,TeacherCourseId) VALUES
 ('2021-03-05',87,6),
 ('2021-04-29',111,3),
 ('2021-12-07',120,1),
-('2021-11-19',107,6)
-INSERT INTO Atendees([Name],Surname,Age) VALUES
+('2021-11-19',107,6),
+('2021-06-19',86,17),
+('2021-10-27',108,18),
+('2021-09-14',94,19),
+('2021-09-01',63,19),
+('2021-03-24',97,20),
+('2021-08-23',115,20),
+('2021-02-23',70,21),
+('2021-05-22',96,21),
+('2021-11-16',115,22),
+('2021-05-07',102,22),
+('2021-07-06',85,23),
+('2021-10-01',117,24),
+('2020-12-16',103,26),
+('2021-11-20',66,25),
+('2021-08-06',118,23),
+('2021-07-08',109,27),
+('2021-10-15',115,28),
+('2021-04-18',117,29),
+('2021-05-09',108,30),
+('2021-11-24',75,31),
+('2021-10-31',77,31),
+('2021-06-15',61,32),
+('2021-02-05',69,33),
+('2021-01-15',76,34),
+('2021-09-04',100,34),
+('2021-01-30',82,35),
+('2021-03-05',87,36),
+('2021-04-29',111,37),
+('2021-12-07',120,38),
+('2021-11-19',107,38)
+INSERT INTO Atendees(AtendeeName,AtendeeSurname,Age) VALUES
 ('Bernadette','Gietz',14),
 ('Nicolette','Row',15),
 ('Malissia','Snoden',50),
@@ -269,11 +339,48 @@ INSERT INTO LectureAtendee(AmountPayed,AtendeeId,LectureId) VALUES
 (5,30,18),
 (4,33,4),
 (3,34,13),
-(2,8,11)
+(2,8,11),
+(0,36,30),
+(0,22,31),
+(0,1,32),
+(1,24,33),
+(1,23,34),
+(1000,7,35),
+(1200,7,36),
+(1250,2,37),
+(80,16,37),
+(50,20,38),
+(30,10,39),
+(50,28,40),
+(7,37,40),
+(0,19,41),
+(3790,7,42),
+(0,8,43),
+(0,34,44),
+(23,1,45),
+(34,38,46),
+(56,10,47),
+(78,32,48),
+(90,1,49),
+(8,35,50),
+(7,36,51),
+(6,19,52),
+(5,30,53),
+(4,33,54),
+(3,34,55),
+(2,8,56),
+(7,36,56),
+(6,19,57),
+(5,30,58),
+(4,33,59),
+(3,34,60),
+(2,8,60)
 
-SELECT [Language],[Date]
+SELECT CourseLanguage,DateOfLecture
 FROM Courses a
 JOIN TeacherCourse b ON a.Id = b.CourseId
 JOIN Lectures c ON b.Id = c.TeacherCourseId
-WHERE [Date] BETWEEN '2020-12-16' AND '2021-10-10'
-GROUP BY [Language]
+WHERE DateOfLecture BETWEEN '2020-12-16' AND '2021-10-10'
+ORDER BY CourseLanguage
+
+SELECT COUNT(*) FROM Lectures
